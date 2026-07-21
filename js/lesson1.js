85d5738e332b16b5ab6080a0ca50971fe7c4e929
sberet <45984213+sberet@users.noreply.github.com>
2026-07-21 13:09:56 -0700

    Add files via upload

---
+++ js/lesson1.js
@@ -0,0 +1,1 @@
+const w=[['Hello','ciao'],['Thank You','grazie']];let i=0,s=0;function sh(){word.textContent=w[i][0];ans.value='';}function check(){msg.textContent=(ans.value.toLowerCase()==w[i][1])?'✅ Correct':'❌ '+w[i][1];if(ans.value.toLowerCase()==w[i][1])s++;i++;score.textContent=`Score: ${s}/${i}`;if(i<w.length)sh();else word.textContent='Finished';}sh();
